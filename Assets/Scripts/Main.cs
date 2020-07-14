20e00806f0bfd1e476134ccefd13aae0becea1be
Giefert <matthewgiefert@gmail.com>
2020-07-14 18:33:37 -0400

    Decoupled state change from physics change
    Code now sends a signal to Update() and FixedUpdate() separately for the player state change vs. the the change in velocity and direction.

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,17 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditorInternal;
 using UnityEngine;
 
 public class Main : MonoBehaviour
 {
-    public delegate void ActionTap();
-    public static event ActionTap onTap;
+    public static event Action onTap;
+    public static event Action onSignal;
+
+    bool shouldSignal = false;
+
+    private void Start()
+    {
+        
+    }
 
     /*
-    * Get key input in Update to avoid input loss that happens in FixedUpdate
-    * Send signal to FixedUpdate to do physics update
-    */
+     * Get key input in Update to avoid input loss that happens in FixedUpdate
+     * Send signal to FixedUpdate to do physics update
+     */
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -19,7 +28,21 @@ public class Main : MonoBehaviour
             if (onTap != null)
             {
                 onTap();
+                shouldSignal = true;
             }
         }
+    }
+
+    private void FixedUpdate()
+    {
+        if (shouldSignal == true)
+        {
+            if (onSignal != null)
+            {
+                onSignal();
+                shouldSignal = false;
+            }
+        }
+
     }
 }
